7f60c50b61caaa4b0443151b1d22a2423447a395
jmasramon <jordi.masramon@gmail.com>
2016-03-25 22:45:51 +0100

    first attempt with state (atom)

--- src/mars_rover_kata/core.clj
+++ src/mars_rover_kata/core.clj
@@ -4,3 +4,73 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(foo "Jordi's")
+
+(def orientations #{:N :S :E :W})
+(def orders #{:f :b :l :r})
+(def grid {:len 10 :hei 10})
+
+(def rover-status-atom (atom {:pos {:x 0 :y 0} :orient :N}))
+
+(defn get-x [status] 
+	(get-in status [:pos :x]))
+
+(defn get-y [status] 
+	(get-in status [:pos :y]))
+
+(defn get-orient [status] 
+	(:orient status))
+
+(defn correct-position? [position]
+	(if (and 
+		(< (:x position) (:len grid))
+		(< (:y position) (:hei grid)))
+			true
+			false))
+
+(defn correct-orientation? [orientation]
+	(if (orientation orientations)
+		true
+		false))
+
+(defn check-status [status]
+	(if (not (correct-position? (:pos status)))
+		false
+		(if (not (correct-orientation? (:orient status)))
+			false
+			true)))
+
+(defn initialize [status]
+	(if (check-status status)
+		(do 
+			(reset! rover-status-atom status)
+			"Roger that!")
+		"Rejected that!"))
+
+(defn get-status []
+	@rover-status-atom)
+
+(defn move [stp]
+	(println "stepping" stp)
+	(case (list (:orient @rover-status-atom) stp)
+		'(:N :f) (do
+			(println "Oriented north go f")
+			(swap! rover-status-atom assoc-in [:pos :y] 1))))
+		; (:N :b) (do
+		; 	(println "Oriented north go b")
+		; 	(swap! rover-status-atom assoc-in [:pos :y] 0))))
+
+(defn do-commands [commands]
+	(println "commands" commands)
+	(doseq [coms commands] (move coms))
+	(get-status))
+
+
+
+
+
+
+
+
+
--- test/mars_rover_kata/core_test.clj
+++ test/mars_rover_kata/core_test.clj
@@ -4,4 +4,52 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest correct-position?-test
+  (testing "correct-position?"
+    (is (correct-position? {:x 0 :y 0}))
+    (is (correct-position? {:x 9 :y 0}))
+    (is (correct-position? {:x 0 :y 9}))
+    (is (not (correct-position? {:x 10 :y 9})))))
+
+(deftest correct-orientation?-test
+  (testing "correct-orientation?"
+    (is (correct-orientation? :S))
+    (is (correct-orientation? :N))
+    (is (correct-orientation? :W))
+    (is (correct-orientation? :E))
+    (is (not (correct-orientation? :K)))))
+
+(deftest initialization
+  (testing "Initializing the rover."
+    (is (= "Roger that!" (initialize {:pos {:x 0 :y 0} :orient :N})))))
+
+(deftest getting-status
+	(testing "Getting statatus.")
+	(is (= {:pos {:x 0 :y 0} :orient :N} (get-status)))
+	(is (not (= {:pos {:x 1 :y 0} :orient :N} (get-status)))))
+
+(deftest initialization-bis
+  (testing "Initializing the rover to a diff status."
+    (is (= "Roger that!" (initialize {:pos {:x 1 :y 1} :orient :S})))
+    (is (= {:pos {:x 1 :y 1} :orient :S} (get-status)))))
+
+(deftest move-forwards
+  (testing "Move forwards."
+  	(initialize {:pos {:x 0 :y 0} :orient :N})
+    (is (= {:pos {:x 0 :y 1} :orient :N} (do-commands '(:f))))))
+
+; (deftest move-backwards
+;   (testing "Move backwards."
+;   	(initialize {:pos {:x 0 :y 1} :orient :N})
+;     (is (= {:pos {:x 0 :y 0} :orient :N} (do-commands '(:b))))))
+
+
+
+
+
+
+
+
+
