8bbb654a100d88bb28327dfe5f57cd89752479b1
jmasramon <jordi.masramon@gmail.com>
2016-03-28 19:33:24 +0200

    refactoring to more explicit data transformation pipelines

--- src/mars_rover_kata/stateless_mars_rover.clj
+++ src/mars_rover_kata/stateless_mars_rover.clj
@@ -18,51 +18,66 @@
 (defn get-orientation [status] 
 	(:orient status))
 
+(defn get-orientation-index [orientation]
+	(orientation orientations-indexes))
+
+(defn calculate-destination [status axis axis-dest]
+	(if (= :x axis) 
+		{:x axis-dest :y (get-axis-value status :y)} 
+		{:x (get-axis-value status :x) :y axis-dest}))
+
+(defn obstacle-in-destination? [destination]
+	(if (contains? obstacles destination)
+		true
+		false))
+
+(defn update-position [status destination]
+	(assoc-in status [:pos] destination))
+
 (defn movement-delta [status command]
 	(* ((get-orientation status) orientations-deltas) 
 		(command movements-deltas)))
 		
-(defn destination [status axis dest]
-	(if (= :x axis) 
-		{:x dest :y (get-axis-value status :y)} 
-		{:x (get-axis-value status :x) :y dest}))
-
-(defn obstacle-in-destination? [status axis dest]
-	(if (contains? obstacles (destination status axis dest))
-		true
-		false))
-
 (defn new-axis-pos [status command axis]
 	(mod (+ (get-axis-value status axis) 
 			(movement-delta status command))
 		grid-size))
 
 (defn set-new-axis-val [status command axis]
-	(let [dest (new-axis-pos status command axis)]
-		(if (obstacle-in-destination? status axis dest) 
+	(let [destination (calculate-destination status axis (new-axis-pos status command axis))]
+		(if (obstacle-in-destination? destination) 
 			status ; better to throw an exception?
-			(assoc-in status [:pos axis] dest))))
+			(update-position status destination))))
 
-(defn new-orientation-index [status command]
-	(mod (+ ((get-orientation status) orientations-indexes) 
-			(command movements-deltas)) 
-		(count orientations)))
+(defn update-orientation [status orientation]
+	(assoc-in status [:orient] orientation))
 
-(defn new-orientation [status command]
-	(get orientations (new-orientation-index status command)))
+(defn get-new-orientation [index]
+	(get orientations index))
 
-(defn set-new-orientation [status command]
-	(assoc-in status [:orient] (new-orientation status command)))
+(defn get-new-orientation-index [status command-delta]
+	(let [new-lin-orientation-ind ((comp (partial + command-delta) 
+										get-orientation-index 
+										get-orientation) 
+									status)]
+		(mod new-lin-orientation-ind (count orientations))))
+
+(defn set-new-orientation [status command-delta]
+	(let [set-new-orientation-fun (comp (partial update-orientation status) 
+										get-new-orientation 
+										get-new-orientation-index)]
+		(set-new-orientation-fun status command-delta)))
 
 (defn move [status command]
-	(let [set-new-axis-val-in (partial set-new-axis-val status command)]
+	(let [command-delta (command movements-deltas)
+		set-new-axis-val-in (partial set-new-axis-val status command)]
 		(case  #{(get-orientation status) command}
 			(#{:N :f} #{:N :b} #{:S :f} #{:S :b}) 
 				(set-new-axis-val-in :y)
 			(#{:E :f} #{:E :b} #{:W :f} #{:W :b}) 
 				(set-new-axis-val-in :x)
 			(#{:N :l} #{:N :r} #{:S :l} #{:S :r} #{:W :l} #{:W :r} #{:E :l} #{:E :r}) 
-				(set-new-orientation status command)
+				(set-new-orientation status command-delta)
 			)))
 
 (defn do-commands [status commands]
